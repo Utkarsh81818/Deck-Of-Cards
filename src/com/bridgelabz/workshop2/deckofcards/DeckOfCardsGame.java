60d23fc1834515c835ff4bacdfcdb80a7c051b0c
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-10-01 08:29:06 +0530

    Added: Welcome Message

---
+++ src/com/bridgelabz/workshop2/deckofcards/DeckOfCardsGame.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.workshop2.deckofcards;
+
+public class DeckOfCardsGame {
+
+    public static void welcome(){
+        System.out.println("Welcome to the gaming world of Deck of Cards");
+    }
+
+    public static void main(String[] args) {
+	welcome();
+    }
+}
