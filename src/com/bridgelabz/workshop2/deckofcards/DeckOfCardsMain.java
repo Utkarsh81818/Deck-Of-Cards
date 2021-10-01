f3136f7f8a0c848e7ef590c49b30629b729e2012
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-10-01 08:54:42 +0530

    Added: Initializing the size of Deck of cards

---
+++ src/com/bridgelabz/workshop2/deckofcards/DeckOfCardsMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.workshop2.deckofcards;
+
+public class DeckOfCardsMain {
+    public static void main(String[] args) {
+        DeckOfCardsGame deckOfCardsGame = new DeckOfCardsGame();
+        deckOfCardsGame.welcome();
+        deckOfCardsGame.deckOfCards();
+    }
+}
